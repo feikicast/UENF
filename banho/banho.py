91b185d637384685eef3f3d9677e246220740081
feikicast <91622496+feikicast@users.noreply.github.com>
2022-03-19 17:41:27 -0300

    bull

--- banho/banho.py
+++ banho/banho.py
@@ -1,165 +1,137 @@
 from time import sleep
-g = d = 0
-b = c = e = f = 'n'
-w1 = 0
-while w1 == 0:
-    def fazer_hoje():
-        print('O que você vai fazer hoje?')
-        print('[1] Não tomar banho')
-        print('[2] Tomar banho')
-        a = int(input(''))
-        if a == '2':
-            w1 = 0
-            sleep(1.5)
-            print('OK, vamos tomar banho')
-            sleep(1.5)
-            print('*pegando a toalha*')
-            sleep(1.5)
-            print('*pegando a roupa*')
-            sleep(1.5)
-            b == 'n'
-            b = input('Pegar o celular? [Sim/Não] ')
-            w2 = 0
-            while w2 == 0:
-                try:
-                    if b.lower() == 's':
-                        sleep(1.5)
-                        print('*pegando o celular*')
-                        sleep(1.5)
-                        e == 'n'
-                        e = input('Você vai ouvir música no banho? [Sim/Não] ')
-                        w3 = 0
-                        while w3 == 0:
-                            try:
-                                if e.lower() == 'n':
-                                    sleep(1.5)
-                                    f == 'n'
-                                    f = input('Você quer colocar o celular pra carregar? [Sim/Não] ')
-                                    w4 = 0
-                                    while w4 == 0:
-                                        try:
-                                            if f.lower() == 'n':
-                                                sleep(1.5)
-                                                print('Pegou celular pra quê então?')
-                                                w4 += 1
-                                                return False
-                                            elif f.lower() == 's':
-                                                sleep(1.5)
-                                                print('\033[31mSeu celular é Samsung, ele explodiu e você morreu.\033[m')
-                                                w1 += 1
-                                                return False
-                                        except:
-                                            print('\033[33mValor inválido, tente novamente.\033[m')
-                                            w4 = 0
-                                if e.lower() == 's':
-                                    sleep(1.5)
-                                    print('Onde você vai deixar o celular?')
-                                    print('[1] Fora do box')
-                                    print('[2] Na báscula/janela')
-                                    g = int(input(''))
-                                    w5 = 0
-                                    while w5 == 0:
-                                        try:
-                                            if g == '2':
-                                                print('\033[31mSeu celular caiu no chão e explodiu, causando a sua morte.\033[m')
-                                                w1 += 1
-                                                return False
-                                            elif g == '1':
-                                                print('Boa escolha.')
-                                                w5 += 1
-                                        except:
-                                            print('\033[33mValor inválido, tente novamente.\033[m')
-                                            w5 = 0
-                                sleep(1.5)
-                                c1 == 's'
-                                c1 = input('Você vai se depilar hoje? [Sim/Não] ')
-                                w6 = 0
-                                while w6 == 0:
-                                    try:
-                                        if c1.lower() == 's':
-                                            sleep(1.5)
-                                            print('O que você vai usar?')
-                                            print('[1] Máquina')
-                                            print('[2] Prestobarba')
-                                            d = int(input(''))
-                                            w6 += 1
-                                        elif c1.lower() == 'n':
-                                            sleep(1.5)
-                                            print('\033[31mSeu pelo encravou, gerando uma infecção que atingiu a corrente sanguínea, você desenvolveu septicemia e morreu.\033[m')
-                                            w1 += 1
-                                            return False
-                                    except:
-                                        print('\033[33mValor inválido, tente novamente.\033[m')
-                                        w6 = 0
-                            except:
-                                print('\033[33mValor inválido, tente novamente.\033[m')
-                                w3 = 0        
-                    elif b.lower() == 'n':
-                        sleep(1.5)
-                        c2 == 's'
-                        c2 = input('Você vai se depilar hoje? [Sim/Não] ')
-                        while w6 == 0:
-                            if c2.lower() == 's':
-                                sleep(1.5)
-                                print('O que você vai usar?')
-                                print('[1] Máquina')
-                                print('[2] Prestobarba')
-                                d = int(input(''))
-                                w6 += 1
-                            if c2.lower() == 'n':
-                                sleep(1.5)
-                                print('\033[31mSeu pelo encravou, gerando uma infecção que atingiu a corrente sanguínea, você desenvolveu septicemia e morreu.\033[m')
-                                w1 += 1
-                                return False
-                except:
-                    print('Valor inválido, tente novamente.')
-                    w2 = 0
-        elif a == '1':
-            sleep(1.5)
-            print('\033[31mApós 1 semana sem tomar banho, você desenvolveu uma infecção e morreu.\033[m')
-            w1 += 1
-            return False
-    def fim():
-        sleep(1.5)
-        if g == '1' and d == '1':
-            sleep(1.5)
-            print('*ligando o chuveiro*')
-            sleep(1.5)
-            print('*se molhando*')
-            sleep(1.5)
-            print('*se ensaboando*')
-            sleep(1.5)
-            print('*ligando a máquina*')
-            sleep(1.5)
-            print('*começando a se depilar*')
-            sleep(1.5)
-            print('*a água entrou na máquina*')
-            sleep(1.5)
-            print('\033[31mVocê morreu eletrocutado.\033[m')
-            w1 += 1
-            return False
-        if g == '1' and d == '2':
-            sleep(1.5)
-            print('*ligando o chuveiro*')
-            sleep(1.5)
-            print('*se molhando*')
-            sleep(1.5)
-            print('*se ensaboando*')
-            sleep(1.5)
-            print('*pegando o prestobarba*')
-            sleep(1.5)
-            print('*começando a se depilar*')
-            sleep(1.5)
-            print('*terminando de se depilar*')
-            sleep(1.5)
-            print('*se limpando*')
-            sleep(1.5)
-            print('*pegando a toalha*')
-            sleep(1.5)
-            print('*se secando*')
-            sleep(1.5)
-            print('*colocando a roupa*')
-            sleep(1.5)
-            print('*FIM*')
-            w1 += 1
-            return False
+def menu_hoje():
+    print('''---------------------
+O que você vai fazer hoje?
+[1] Não tomar banho
+[2] Tomar banho
+---------------------''')
+
+def menu_cel():
+    print('''----------------------
+Onde você vai deixar o celular?
+[1] Fora do box
+[2] Na báscula/janela
+----------------------''')   
+
+def depilar():
+    print('''----------------------
+O que você vai usar?
+[1] Máquina
+[2] Prestobarba
+----------------------''') 
+
+def main():
+    menu_hoje()
+    escolha = int(input(" "))
+    if escolha == 2:
+        sleep(1)
+        print('OK, vamos tomar banho')
+        sleep(1)
+        print('*pegando a toalha*')
+        sleep(1)
+        print('*pegando a roupa*')
+        sleep(1)
+        while True:
+            resp_celular = str(input("Você quer pegar seu celular? [S/N] ")).upper()[0]
+            if resp_celular == 'S' or resp_celular == 'N':
+                break
+            else:
+                print('Digite apenas S ou N')
+        if resp_celular == 'S':
+            sleep(1)
+            print("*pegando celular*")
+            while True:
+                sleep(1)
+                resp_musica = str(input('Você vai ouvir música no banho? [S/N] ')).upper()[0]  
+                if resp_musica == 'S' or resp_musica == 'N':
+                    break
+                else:
+                    print('Digite apenas S ou N')
+            if resp_musica == 'S':
+                menu_cel()
+                while True:
+                    resp_deixar_cel = int(input(' '))
+                    if resp_deixar_cel == 1 or resp_deixar_cel == 2:
+                        break
+                    else:
+                        print('Digite apenas 1 ou 2')
+                if resp_deixar_cel == 2:
+                    sleep(1)
+                    print('\033[31mSeu celular caiu no chão e explodiu, causando a sua morte.\033[m')
+                    exit()
+                else:
+                    sleep(1)
+                    print('Boa escolha.')    
+            elif resp_musica == 'N':
+                while True:
+                    resp_carregar = str(input('Você quer colocar o celular pra carregar? [S/N] ')).upper()[0]
+                    if resp_carregar == 'S' or resp_musica == 'N':
+                        break
+                    else:
+                        print('Digite apenas S ou N')
+                if resp_carregar == 'S':
+                    sleep(1)
+                    print('\033[31mSeu celular explodiu e você morreu.\033[m')
+                    exit()
+                elif resp_carregar == 'N':
+                    sleep(1)
+                    print('Pegou o celular pra quê então?')
+            resp_depilar = str(input('Você vai se depilar hoje? [S/N] ')).upper()[0]
+            if resp_depilar == 'S':
+                sleep(1)
+                depilar()
+                while True:
+                    resp_depilar_12 = int(input(' '))
+                    if resp_depilar_12 == 1 or resp_depilar_12 == 2:
+                        break
+                if resp_depilar_12 == 1:
+                    sleep(1)
+                    print('\033[31mAo entrar em contato com a água, a máquina disparou um descarga elétrica e te matou\033[m')
+                elif resp_depilar_12 == 2:
+                    sleep(1)
+                    print('Boa escolha')
+                    sleep(1)
+                    print('\033[32mVocê encontrou 1 dos 3 finais alternativos em que sairia vivo, será que é capaz de encontrar todos eles?\033[m')
+                else:
+                    print('Digite apenas 1 ou 2')
+            elif resp_depilar == 'N':
+                sleep(1)
+                print('\033[31mSeu pelo encravou, gerando uma infecção que atingiu a corrente sanguínea, você desenvolveu septicemia e morreu.\033[m')
+            else:
+                print('Digite apenas S ou N')
+        elif resp_celular == 'N':
+            sleep(1)
+            resp_depilar = str(input('Você vai se depilar hoje? [S/N] ')).upper()[0]
+            if resp_depilar == 'S':
+                sleep(1)
+                depilar()
+                while True:
+                    resp_depilar_12 = int(input(' '))
+                    if resp_depilar_12 == 1 or resp_depilar_12 == 2:
+                        break
+                if resp_depilar_12 == 1:
+                    sleep(1)
+                    print('\033[31mAo entrar em contato com a água, a máquina disparou um descarga elétrica e te matou\033[m')
+                elif resp_depilar_12 == 2:
+                    sleep(1)
+                    print('Boa escolha')
+                    sleep(1)
+                    print('\033[32mVocê encontrou 1 dos 3 finais alternativos em que sairia vivo, será que é capaz de encontrar todos eles?\033[m')
+                else:
+                    print('Digite apenas 1 ou 2')
+            elif resp_depilar == 'N':
+                sleep(1)
+                print('\033[31mSeu pelo encravou, gerando uma infecção que atingiu a corrente sanguínea, você desenvolveu septicemia e morreu.\033[m')
+            else:
+                print('Digite apenas S ou N')
+    elif escolha == 1:
+        sleep(1)
+        print('\033[31mApós 1 semana sem tomar banho, você desenvolveu uma infecção e morreu.\033[m')
+    elif escolha == 999:
+        sleep(1)
+        print('OK, então volte a assistir TV')
+        sleep(3)
+        print('\033[032mCaramba, você é bom! Você encontrou o final secreto!!\033[m')
+    else:
+        print('Digite apenas 1 ou 2')
+main()
